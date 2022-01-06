7ace2de08cdb5c37500dae7c5e04e01170075f66
nico <nico.campolongo@spanflug.de>
2022-01-06 18:07:22 +0100

    Adding day 25

--- aoc21/day25/part1.py
+++ aoc21/day25/part1.py
@@ -1,26 +1,74 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
+
 from aocd.models import Puzzle
+from tqdm import trange
 
 
-def solve(data):
-    return -1
+def move(row, where):
+    new_row = deepcopy(row)
+    for loc, couple in enumerate(zip(row, row[1:])):
+        c1, c2 = couple
+        if c1 == where and c2 == ".":
+            new_row[loc] = "."
+            new_row[loc + 1] = where
+    if row[-1] == where and row[0] == ".":
+        new_row[-1] = "."
+        new_row[0] = where
+    return new_row
+
+
+def solve(input_f=None, debug=False):
+    if not input_f:
+        puzzle = Puzzle(year=2021, day=25)
+        data = puzzle.input_data.split("\n")
+    else:
+        data = open(input_f).readlines()
+    processed_data = []
+    for line in data:
+        processed_data.append([i for i in line.strip()])
+
+    data = processed_data
+    if debug:
+        for row in data:
+            print("".join(row))
+        print()
+
+    rows = len(data)
+    cols = len(data[0])
+    for step in trange(1000):
+        if debug:
+            print(f"After step {step+1}:")
+        new_data = [["."] * cols for _ in range(rows)]
+        # move left
+        for pos, row in enumerate(data):
+            new_row = move(row, ">")
+            new_data[pos] = new_row
+
+        # move down
+        for j in range(cols):
+            col = [new_data[i][j] for i in range(rows)]
+            new_col = move(col, "v")
+            # update col
+            for i in range(rows):
+                new_data[i][j] = new_col[i]
+
+        if data == new_data:
+            break
+        data = new_data
+
+        if debug:
+            for row in data:
+                print("".join(row))
+            print()
+    return step + 1
 
 
 if __name__ == "__main__":
     # test_puzzle
-    test_data = []
-    with open("test.txt") as f:
-        for line in f:
-            test_data.append(line.strip())
-
-    res = solve(test_data)
+    res = solve("test.txt")
     print("part1: ", res)
 
     # real puzzle
-    processed_data = []
-    puzzle = Puzzle(year=2022, day=25)
-    for line in puzzle.input_data.split("\n"):
-        processed_data.append([int(i) for i in line.split(",")])
-
-    res = solve(processed_data)
+    res = solve()
     print(res)
